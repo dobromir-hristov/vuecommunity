4d9742a85efe46bc859a286cee962d8909e752bb
Dobromir Hristov <dobromir92@gmail.com>
2019-03-20 00:36:09 +0200

    feat: add useful links component

---
+++ docs/.vuepress/components/UsefulLinks.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="UsefulLinks">
+    <slot/>
+  </div>
+</template>
+
+<script>
+/**
+ * @module UsefulLinks
+ */
+export default {
+  name: 'UsefulLinks'
+}
+</script>
+
+<style>
+.UsefulLinks {
+  border: 1px solid #f0f0f0;
+  padding: 1rem;
+  padding-left: 0;
+  border-radius: 4px;
+  background: linear-gradient(#fff 40%, #f3f3f3);
+  color: #6c6c6c;
+  box-shadow: 0 4px 11px 0 hsla(233, 45%, 26%, 0.15), 0 1px 3px 0 hsla(232, 23%, 47%, 0.2)
+}
+</style>
---
+++ docs/.vuepress/components/UsefulLinksSection.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="UsefulLinksSection">
+    <div class="UsefulLinksSection__title">
+      {{ title }}
+    </div>
+    <div class="UsefulLinksSection__content">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+/**
+ * @module UsefulLinksSection
+ */
+export default {
+  name: 'UsefulLinksSection',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../theme/utils";
+
+.UsefulLinksSection {
+  display: flex;
+  flex-flow: column wrap;
+  margin-bottom: $spacing;
+  align-items: flex-start;
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+
+  &__title {
+    font-weight: bold;
+    border-left: 4px solid #3eaf7c;
+    padding-left: 0.65rem;
+    margin-bottom: $spacing;
+  }
+
+  &__content {
+    padding-left: $spacing-small;
+
+    p {
+      margin: 0;
+      line-height: 1;
+
+      & + p {
+        margin-top: $spacing-small;
+        padding-top: $spacing-small;
+        border-top: 1px solid #e3e3e3;
+      }
+    }
+
+    a .icon.outbound {
+      margin-left: $spacing-tiny/2;
+    }
+
+    ul {
+      line-height: 1;
+      margin: 0;
+      list-style-type: none;
+      padding: 0;
+
+      li {
+        margin-bottom: $spacing-small;
+        padding-bottom: $spacing-small;
+        border-bottom: 1px solid #e3e3e3;
+
+        &:last-of-type {
+          border-bottom: none;
+          margin-bottom: 0;
+          padding-bottom: 0;
+        }
+      }
+    }
+  }
+
+  @include tablet-landscape-and-up {
+    flex-flow: row;
+    /*margin-bottom: 0;*/
+    &__title {
+      flex: 1 1 30%;
+      margin-bottom: 0;
+    }
+
+    &__content {
+      flex: 1 1 70%;
+    }
+  }
+}
+</style>
