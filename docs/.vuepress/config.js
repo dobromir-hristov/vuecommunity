039a636eb32a6c25a1aecb3755ec434a10e1b39d
Dobromir Hristov <dobromir92@gmail.com>
2019-05-19 10:51:04 +0300

    feat: add logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,6 +16,7 @@ module.exports = {
   ],
   // serviceWorker: true,
   themeConfig: {
+    logo: '/logo_pin.png',
     docsDir: 'docs',
     repo: 'https://github.com/dobromir-hristov/vuecommunity',
     editLinks: true,
@@ -95,5 +96,17 @@ module.exports = {
         ]
       }
     }
-  }
+  },
+  head: [
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type: 'image/,png', sizes: '32x32', href: '/favicons/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/,png', sizes: '16x16', href: '/favicons/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/favicons/site.webmanifest' }],
+    ['link', { rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg', color: '#5bbad5' }],
+    ['link', { rel: 'shortcut icon', href: '/favicons/favicon.ico' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#ffffff' }],
+    ['meta', { name: 'msapplication-config', content: '/favicons/browserconfig.xml' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }]
+
+  ]
 }
